dcb03bcf855bd7a7b96a13a92facadaabcdf4c49
ocd0711 <ocd2057try@gmail.com>
2024-08-20 10:00:16 +0800

    overlay kebab euicc
    Change-Id: I8b70818d4dccc82c7e671e30c49b9ec93e206fae

--- device.mk
+++ device.mk
@@ -55,3 +55,6 @@ $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/kebab/kebab-vendor.mk)
+
+# Add Gapps
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
